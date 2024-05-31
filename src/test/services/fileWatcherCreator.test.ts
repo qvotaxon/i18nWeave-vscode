00091c1b74320dcbf20d96c7521f792b5f2791dc
Jordy Vervloed <jvervloed@rgfstaffing.nl>
2024-05-31 22:52:41 +0200

    refactor(test): simplify fileWatcherCreator tests

--- src/test/services/fileWatcherCreator.test.ts
+++ src/test/services/fileWatcherCreator.test.ts
@@ -2,9 +2,7 @@ import * as assert from 'assert';
 import FileWatcherCreator from '../../services/fileWatcherCreator';
 import * as mock from 'mock-fs';
 import JsonFileChangeHandler from '../../fileChangeHandlers/jsonFileChangeHandler';
-import { Uri } from 'vscode';
 import FileChangeHandlerFactory from '../../services/fileChangeHandlerFactory';
-import { verify } from 'crypto';
 
 suite('FileWatcherCreator Tests', () => {
   let tracker: assert.CallTracker;
@@ -18,16 +16,13 @@ suite('FileWatcherCreator Tests', () => {
   });
 
   test('createFileWatchersForFilesMatchingGlobAsync should create file watchers for files matching the glob pattern', async () => {
-    const expectedFileWatchersCount = 6;
-
     const pattern = '**/*.json';
     const disableFlags: (() => boolean)[] = [];
 
     const fileWatcherCreator = new FileWatcherCreator();
 
     const callsfunc = tracker.calls(
-      FileChangeHandlerFactory.prototype.createFileChangeHandler,
-      expectedFileWatchersCount
+      FileChangeHandlerFactory.prototype.createFileChangeHandler
     );
 
     FileChangeHandlerFactory.prototype.createFileChangeHandler = callsfunc;
@@ -39,13 +34,10 @@ suite('FileWatcherCreator Tests', () => {
         ...disableFlags
       );
 
-    assert.equal(fileWatchers.length, expectedFileWatchersCount);
-    tracker.verify();
+    assert.equal(tracker.getCalls(callsfunc).length, fileWatchers.length);
   });
 
   test('createFileWatchersForFilesMatchingGlobAsync should disable file watchers based on disable flags', async () => {
-    const expectedFileWatchersCount = 6;
-
     const pattern = '**/*.json';
     const disableFlags: (() => boolean)[] = [() => true, () => false];
 
@@ -58,13 +50,11 @@ suite('FileWatcherCreator Tests', () => {
 
     tracker.reset(callsfunc);
 
-    const fileWatchers =
-      await fileWatcherCreator.createFileWatchersForFilesMatchingGlobAsync(
-        pattern,
-        ...disableFlags
-      );
+    await fileWatcherCreator.createFileWatchersForFilesMatchingGlobAsync(
+      pattern,
+      ...disableFlags
+    );
 
-    assert.equal(fileWatchers.length, expectedFileWatchersCount);
     assert.equal(tracker.getCalls(callsfunc).length, 0);
   });
 });
