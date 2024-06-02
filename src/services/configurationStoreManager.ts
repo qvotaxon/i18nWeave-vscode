17865a08fb769ac040edcff1db7d783b6aaae6c7
Jordy Vervloed <jvervloed@rgfstaffing.nl>
2024-06-02 15:26:00 +0200

    fix: use hasOwnProperty in config store
    Prevent Prototype-polluting function issue: 
    The property chain  is recursively assigned to  without guarding against prototype pollution.

--- src/services/configurationStoreManager.ts
+++ src/services/configurationStoreManager.ts
@@ -90,7 +90,7 @@ export default class ConfigurationStoreManager {
     let current = obj;
 
     for (let i = 0; i < keys.length - 1; i++) {
-      if (!current[keys[i]]) {
+      if (!current.hasOwnProperty(keys[i])) {
         current[keys[i]] = {};
       }
       current = current[keys[i]];
