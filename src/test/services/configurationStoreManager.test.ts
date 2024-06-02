ec1c3f1ed90da764ca1f6829fa809e7d73723fe7
Jordy Vervloed <jvervloed@rgfstaffing.nl>
2024-06-02 17:42:15 +0200

    feat: enhance config store init and error handling
    - Rewrite Initialize suite to include configuration subscription and
      initialization logic
    - Add new tests for error scenarios in syncConfigurationStore, ensuring
      coverage for missing configurations or values
    - Implement getConfig tests for better error handling and fetching of
      specific configurations
    - Update nl/common.po file timestamps to reflect recent changes
    - Import ExtensionConfiguration and TranslationModuleConfiguration for
      improved type support and clarity in config management logic

--- src/test/services/configurationStoreManager.test.ts
+++ src/test/services/configurationStoreManager.test.ts
@@ -3,6 +3,8 @@ import * as vscode from 'vscode';
 import sinon from 'sinon';
 import ConfigurationStore from '../../services/configurationStore';
 import ConfigurationStoreManager from '../../services/configurationStoreManager';
+import ExtensionConfiguration from '../../entities/configuration/extensionConfiguration';
+import TranslationModuleConfiguration from '../../entities/configuration/modules/translationModule/translationModuleConfiguration';
 
 suite('ConfigurationStoreManager', () => {
   let configurationStoreManager: ConfigurationStoreManager;
@@ -24,16 +26,8 @@ suite('ConfigurationStoreManager', () => {
     sinon.restore();
   });
 
-  suite('syncConfigurationStore', () => {
-    test('should throw an error if configuration is not found', () => {
-      getExtensionStub.returns(undefined);
-
-      assert.throws(() => {
-        configurationStoreManager['syncConfigurationStore']();
-      }, Error('Configuration not found.'));
-    });
-
-    test('should correctly sync the configuration store', () => {
+  suite('Initialize', () => {
+    test('should call syncConfigurationStore and subscribe to configuration changes', () => {
       const mockConfig = {
         id: 'translationModule',
         properties: {
@@ -51,6 +45,57 @@ suite('ConfigurationStoreManager', () => {
       };
       getExtensionStub.returns({ packageJSON: mockContributes });
 
+      const getMockConfig = {
+        get: sinon.stub().returns('configValue'),
+      };
+      getConfigurationStub.returns(getMockConfig);
+
+      configurationStoreManager.Initialize();
+
+      sinon.assert.calledOnce(onDidChangeConfigurationStub);
+    });
+  });
+
+  suite('syncConfigurationStore', () => {
+    setup(() => {
+      const mockConfig = {
+        id: 'translationModule',
+        properties: {
+          i18nWeave: {
+            translationModule: {
+              enabled: true,
+            },
+          },
+        },
+      };
+      const mockContributes = {
+        contributes: {
+          configuration: [mockConfig],
+        },
+      };
+      getExtensionStub.returns({ packageJSON: mockContributes });
+    });
+
+    test('should throw an error if configuration is not found', () => {
+      getExtensionStub.returns(undefined);
+
+      assert.throws(() => {
+        configurationStoreManager['syncConfigurationStore']();
+      }, Error('Configuration not found.'));
+    });
+
+    test('should throw an error if configuration value is missing', () => {
+      const getMockConfig = {
+        get: sinon.stub().returns(undefined),
+      };
+      getConfigurationStub.returns(getMockConfig);
+
+      assert.throws(() => {
+        configurationStoreManager['syncConfigurationStore']();
+      }, Error('Configuration value not found for key: i18nWeave'));
+    });
+
+    test('should correctly sync the configuration store', () => {
       const getMockConfig = {
         get: sinon.stub().returns('configValue'),
       };
@@ -104,24 +149,30 @@ suite('ConfigurationStoreManager', () => {
   });
 
   suite('getConfig', () => {
-    // it('should throw an error if the specific configuration is not found', () => {
-    //   const mockConfigStore = new ConfigurationStore(
-    //     {}
-    //   );
-    //   configurationStoreManager['_configurationStore'] = mockConfigStore;
-    //   assert.throws(() => {
-    //     configurationStoreManager.getConfig('mockKey');
-    //   }, 'Configuration for key "mockKey" not found.');
-    // });
-    // it('should return the specific configuration if found', () => {
-    //   const mockConfigStore = new ConfigurationStore({
-    //     mockKey: 'mockValue' as any,
-    //   });
-    //   configurationStoreManager['_configurationStore'] = mockConfigStore;
-    //   assert.strictEqual(
-    //     configurationStoreManager.getConfig('mockKey'),
-    //     'mockValue'
-    //   );
-    // });
+    test('should throw an error if configuration for key is not found', () => {
+      const mockConfigStore = new ConfigurationStore({});
+      configurationStoreManager['_configurationStore'] = mockConfigStore;
+
+      assert.throws(() => {
+        configurationStoreManager.getConfig(
+          'nonExistentKey' as keyof ExtensionConfiguration
+        );
+      }, Error('Configuration for key "nonExistentKey" not found.'));
+    });
+
+    test('should return the configuration for the specified key', () => {
+      const translationModuleConfiguration =
+        new TranslationModuleConfiguration();
+      translationModuleConfiguration.enabled = true;
+
+      const mockConfigStore = new ConfigurationStore({
+        translationModule: translationModuleConfiguration,
+      });
+      configurationStoreManager['_configurationStore'] = mockConfigStore;
+
+      const result =
+        configurationStoreManager.getConfig<string>('translationModule');
+      assert.strictEqual(result, translationModuleConfiguration);
+    });
   });
 });
