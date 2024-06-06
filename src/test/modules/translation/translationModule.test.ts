62a049965b369ca84a66d455a62e21624f7ec71f
Jordy <jordy.vervloed@gmail.com>
2024-06-06 22:46:04 +0200

    test(translation): Implement json content translation (#20)
    Unit tests are added to validate both the creation of 
    an empty translations object and the update functionality of 
    jsonContent with translated content. This ensures that the 
    TranslationModule behaves as expected when handling json 
    translations.

---
+++ src/test/modules/translation/translationModule.test.ts
@@ -0,0 +1,49 @@
+import * as assert from 'assert';
+import { Uri } from 'vscode';
+import TranslationModule from '../../../modules/translation/translationModule';
+
+suite('TranslationModule', () => {
+  test('should create an empty translations object if jsonContent exists', async () => {
+    const inputPath = Uri.file('/path/to/input.json');
+    const translationModule = new TranslationModule();
+    const context = {
+      inputPath,
+      jsonContent: {},
+      outputPath: Uri.file(''),
+      locale: '',
+      translations: {}, // Add the 'translations' property with an initial value of an empty object
+    };
+
+    await translationModule.execute(context);
+
+    assert.deepStrictEqual(context.translations, {});
+  });
+
+  test('should update jsonContent with translatedJsonContent if jsonContent exists', async () => {
+    const inputPath = Uri.file('/path/to/input.json');
+    const translationModule = new TranslationModule();
+    const context = {
+      inputPath,
+      jsonContent: {
+        dummy: {
+          translations: {
+            test: 'original',
+          },
+        },
+      },
+      outputPath: Uri.file(''),
+      locale: '',
+      translations: {},
+    };
+
+    await translationModule.execute(context);
+
+    assert.deepStrictEqual(context.jsonContent, {
+      dummy: {
+        translations: {
+          test: 'test',
+        },
+      },
+    });
+  });
+});
