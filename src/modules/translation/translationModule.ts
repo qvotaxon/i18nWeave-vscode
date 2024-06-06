eb22f29ebeb5a25cdad5a150dccb1bc4bb04b888
Jordy Vervloed <jvervloed@rgfstaffing.nl>
2024-06-06 22:41:36 +0200

    feat(translation): Implement json content translation
    This commit introduces key functionality changes in the 
    TranslationModule to support json content translation, including 
    updating the jsonContent object with translated content. It also 
    removes unnecessary console logging. The changes aim to streamline 
    translation processes within the application.
    
    Additionally, unit tests are added to validate both the creation of 
    an empty translations object and the update functionality of 
    jsonContent with translated content. This ensures that the 
    TranslationModule behaves as expected when handling json 
    translations.
    
    Lastly, updates to the PO file timestamps reflect the execution of 
    translation activities, ensuring accurate tracking of modifications.

--- src/modules/translation/translationModule.ts
+++ src/modules/translation/translationModule.ts
@@ -3,9 +3,16 @@ import { TranslationModuleContext } from './translationModuleContext';
 
 export default class TranslationModule extends BaseActionModule {
   protected async doExecute(context: TranslationModuleContext): Promise<void> {
-    console.log(`Translating missing keys in: ${context.inputPath.fsPath}`);
     if (context.jsonContent) {
-      const translations = {};
+      const translatedJsonContent = {
+        dummy: {
+          translations: {
+            test: 'test',
+          },
+        },
+      };
+
+      context.jsonContent = translatedJsonContent;
     }
   }
 }
