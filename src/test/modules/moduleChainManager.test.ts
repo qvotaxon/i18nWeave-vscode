dc8ef78bf940fa44239a96f8f1dcc9bc52a91373
Jordy <jordy.vervloed@gmail.com>
2024-06-07 14:43:47 +0200

    feat: implement ModuleChainManager with tests (#25)
    Add ModuleChainManager class to manage module chains based on
    chain types. This includes registering and executing modules for
    specific chain types, which allows for modular and extensible
    chain processing. Add unit tests to validate the implementation,
    including registration and conditional execution based on chain
    type presence.

---
+++ src/test/modules/moduleChainManager.test.ts
@@ -0,0 +1,76 @@
+import assert from 'assert';
+import { ChainType } from '../../enums/chainType';
+import ActionModule from '../../interfaces/actionModule';
+import ModuleContext from '../../interfaces/moduleContext';
+import ModuleChainManager from '../../modules/moduleChainManager';
+import { Uri } from 'vscode';
+
+suite('ModuleChainManager Tests', () => {
+  let moduleChainManager: ModuleChainManager;
+
+  setup(() => {
+    moduleChainManager = new ModuleChainManager();
+  });
+
+  test('registerChain should register a module for a specific chain type', () => {
+    const chainType = ChainType.Json;
+    const actionModule: ActionModule = {
+      execute: async (context: ModuleContext): Promise<void> => {},
+      setNext: function (module: ActionModule | null): void {
+        throw new Error('Function not implemented.');
+      },
+    };
+
+    moduleChainManager.registerChain(chainType, actionModule);
+
+    const chain = moduleChainManager['chains'][chainType];
+    assert.strictEqual(chain, actionModule);
+  });
+
+  test('executeChain should execute the module chain for a specific chain type', () => {
+    const chainType = ChainType.Json;
+    const moduleContext: ModuleContext = {
+      inputPath: Uri.file(''),
+      outputPath: Uri.file(''),
+      locale: '',
+    };
+    let isExecuted = false;
+    const actionModule: ActionModule = {
+      execute: async (context: ModuleContext): Promise<void> => {
+        assert.strictEqual(context, moduleContext);
+        isExecuted = true;
+      },
+      setNext: function (module: ActionModule | null): void {
+        throw new Error('Function not implemented.');
+      },
+    };
+    moduleChainManager.registerChain(chainType, actionModule);
+
+    moduleChainManager.executeChain(chainType, moduleContext);
+
+    assert.strictEqual(isExecuted, true);
+  });
+
+  test('executeChain should not execute the module chain if it is not registered', () => {
+    const nonExistingChainType = 999 as ChainType;
+    const moduleContext: ModuleContext = {
+      inputPath: Uri.file(''),
+      outputPath: Uri.file(''),
+      locale: '',
+    };
+    let isExecuted = false;
+    const actionModule: ActionModule = {
+      execute: async (context: ModuleContext): Promise<void> => {
+        isExecuted = true;
+      },
+      setNext: function (module: ActionModule | null): void {
+        throw new Error('Function not implemented.');
+      },
+    };
+    moduleChainManager.registerChain(ChainType.Json, actionModule);
+
+    moduleChainManager.executeChain(nonExistingChainType, moduleContext);
+
+    assert.strictEqual(isExecuted, false);
+  });
+});
