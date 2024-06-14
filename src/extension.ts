231dd611a9027d93f230e2b10f330bb23acfbd34
Jordy Vervloed <jvervloed@rgfstaffing.nl>
2024-06-14 20:17:21 +0200

    feat: add Sentry profiling support
    Add @sentry/profiling-node to extend monitoring with profiling
    capabilities. Set up Sentry initialization logic in a dedicated
    function to improve clarity. Update GitHub Actions build config
    to include Sentry authentication token.

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,18 @@
 import * as Sentry from '@sentry/node';
-// import { Integrations } from '@sentry/tracing';
 import { ExtensionContext } from 'vscode';
 
 import ConfigurationStoreManager from './services/configurationStoreManager';
 import FileWatcherCreator from './services/fileWatcherCreator';
+
+function initializeSentry() {
+  Sentry.init({
+    dsn: 'https://188de1d08857e4d1a5e59d8a9da5da1a@o4507423909216256.ingest.de.sentry.io/4507431475019856',
+    integrations: Sentry.getDefaultIntegrations({}),
+    tracesSampleRate: 1.0,
+    profilesSampleRate: 1.0,
+  });
+}
+initializeSentry();
 
 export async function activate(
   context: ExtensionContext,
@@ -11,47 +20,39 @@ export async function activate(
 ) {
   console.log('i18nWeave is now active!');
 
-  // Initialize Sentry for Node.js environment
-  Sentry.init({
-    dsn: 'https://188de1d08857e4d1a5e59d8a9da5da1a@o4507423909216256.ingest.de.sentry.io/4507431475019856',
-    integrations: [
-      // nodeProfilingIntegration(),
-    ],
-    // Performance Monitoring
-    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  try {
+    const typeScriptFileGlobPattern = '**/{apps,libs}/**/*.{tsx,ts}';
+    const jsonFileGlobPattern = `**/locales/**/*.json`;
+    const poFileGlobPattern = `**/locales/**/*.po`;
 
-    // Set sampling rate for profiling - this is relative to tracesSampleRate
-    profilesSampleRate: 1.0,
-  });
-  const typeScriptFileGlobPattern = '**/{apps,libs}/**/*.{tsx,ts}';
-  const jsonFileGlobPattern = `**/locales/**/*.json`;
-  const poFileGlobPattern = `**/locales/**/*.po`;
+    const typeScriptFileWatchers = await createWatchersForPattern(
+      typeScriptFileGlobPattern,
+      'i18nextScannerModule',
+      fileWatcherCreator
+    );
 
-  const typeScriptFileWatchers = await createWatchersForPattern(
-    typeScriptFileGlobPattern,
-    'i18nextScannerModule',
-    fileWatcherCreator
-  );
+    const jsonFileWatchers = await createWatchersForPattern(
+      jsonFileGlobPattern,
+      'i18nextJsonToPoConversionModule',
+      fileWatcherCreator
+    );
 
-  const jsonFileWatchers = await createWatchersForPattern(
-    jsonFileGlobPattern,
-    'i18nextJsonToPoConversionModule',
-    fileWatcherCreator
-  );
+    const poFileWatchers = await createWatchersForPattern(
+      poFileGlobPattern,
+      'i18nextJsonToPoConversionModule',
+      fileWatcherCreator
+    );
 
-  const poFileWatchers = await createWatchersForPattern(
-    poFileGlobPattern,
-    'i18nextJsonToPoConversionModule',
-    fileWatcherCreator
-  );
+    ConfigurationStoreManager.getInstance().initialize();
 
-  ConfigurationStoreManager.getInstance().initialize();
-
-  context.subscriptions.push(
-    ...typeScriptFileWatchers,
-    ...jsonFileWatchers,
-    ...poFileWatchers
-  );
+    context.subscriptions.push(
+      ...typeScriptFileWatchers,
+      ...jsonFileWatchers,
+      ...poFileWatchers
+    );
+  } catch (e) {
+    Sentry.captureException(e);
+  }
 }
 
 async function createWatchersForPattern(
