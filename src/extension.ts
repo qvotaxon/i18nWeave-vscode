2cbef63e86401ee7008c63929cc2ec28ca5edda8
Jordy Vervloed <jvervloed@rgfstaffing.nl>
2024-06-14 19:32:45 +0200

    feat: integrate Sentry
    - Integrate Sentry for Node.js environment to monitor and track
    performance with a 100% sampling rate. 
    - Update tsconfig with 
    CommonJS module, ES6 library, and other enhanced compiler options 
    to support Sentry.

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,5 @@
+import * as Sentry from '@sentry/node';
+// import { Integrations } from '@sentry/tracing';
 import { ExtensionContext } from 'vscode';
 
 import ConfigurationStoreManager from './services/configurationStoreManager';
@@ -9,6 +11,18 @@ export async function activate(
 ) {
   console.log('i18nWeave is now active!');
 
+  // Initialize Sentry for Node.js environment
+  Sentry.init({
+    dsn: 'https://188de1d08857e4d1a5e59d8a9da5da1a@o4507423909216256.ingest.de.sentry.io/4507431475019856',
+    integrations: [
+      // nodeProfilingIntegration(),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+
+    // Set sampling rate for profiling - this is relative to tracesSampleRate
+    profilesSampleRate: 1.0,
+  });
   const typeScriptFileGlobPattern = '**/{apps,libs}/**/*.{tsx,ts}';
   const jsonFileGlobPattern = `**/locales/**/*.json`;
   const poFileGlobPattern = `**/locales/**/*.po`;
