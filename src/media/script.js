e9d595aefd63091242cc6fec2778ca0e774dd13c
Jordy <jordy.vervloed@gmail.com>
2024-06-21 15:25:34 +0200

    feat: Add filter and keyboard shortcuts (#99)
    Now you can quickly filter JSON data right from the table view!
    Just start typing in the new filter input box and voila! We’ve also
    added handy keyboard shortcuts: Ctrl+S to save and Ctrl+F to focus
    the filter input. Hit Escape to clear the filter effortlessly. Your
    JSON editing just got a whole lot smoother!

--- src/media/script.js
+++ src/media/script.js
@@ -1,14 +1,48 @@
 const vscode = acquireVsCodeApi();
+const filterInput = document.getElementById('filterInput');
+const jsonTable = document.getElementById('jsonTable');
+const rows = jsonTable.getElementsByTagName('tr');
 
-document.getElementById('saveButton').addEventListener('click', () => {
-    const table = document.getElementById('jsonTable');
-    const jsonData = {};
-    for (let i = 1, row; row = table.rows[i]; i++) {
-        const key = row.cells[0].innerText;
-        const value = row.cells[1].innerText;
-        setNestedValue(jsonData, key.split('.'), value);
+document.getElementById('saveButton').addEventListener('click', () => save());
+
+document.addEventListener('keydown', function (event) {
+    if (event.ctrlKey && event.key === 's') {
+        event.preventDefault();
+        save();
     }
-    vscode.postMessage({ command: 'save', jsonData: JSON.stringify(jsonData, null, 2) });
+
+    if (event.ctrlKey && event.key === 'f') {
+        event.preventDefault();
+        focusFilterInput();
+    }
+
+    if (event.key === 'Escape') {
+        event.preventDefault();
+
+        if (filterInput === document.activeElement) {
+            clearFilterInput();
+            blurFilterInput();
+        }
+    }
+});
+
+filterInput.addEventListener('keyup', function () {
+    const filterValue = filterInput.value.toLowerCase();
+
+    const table = document.getElementById('jsonTable');
+    for (let i = 1, row; row = table.rows[i]; i++) {
+        const keyColumn = rows[i].getElementsByTagName('td')[0];
+        const valueColumn = rows[i].getElementsByTagName('td')[1];
+
+        if (
+            keyColumn.innerHTML.toLowerCase().indexOf(filterValue) > -1 ||
+            valueColumn.innerHTML.toLowerCase().indexOf(filterValue) > -1
+        ) {
+            rows[i].style.display = '';
+        } else {
+            rows[i].style.display = 'none';
+        }
+    }
 });
 
 /**
@@ -22,4 +56,28 @@ function setNestedValue(obj, path, value) {
     const lastKey = path.pop();
     const lastObj = path.reduce((obj, key) => obj[key] = obj[key] || {}, obj);
     lastObj[lastKey] = value;
+}
+
+function save() {
+    const table = document.getElementById('jsonTable');
+    const jsonData = {};
+    for (let i = 1, row; row = table.rows[i]; i++) {
+        const key = row.cells[0].innerText;
+        const value = row.cells[1].innerText;
+        setNestedValue(jsonData, key.split('.'), value);
+    }
+    vscode.postMessage({ command: 'save', jsonData: JSON.stringify(jsonData, null, 2) });
+}
+
+function focusFilterInput() {
+    filterInput.focus();
+}
+
+function blurFilterInput() {
+    filterInput.blur();
+}
+
+function clearFilterInput() {
+    filterInput.value = '';
+    filterInput.dispatchEvent(new Event('keyup'));
 }
