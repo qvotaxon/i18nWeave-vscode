c038df897bfe06732e04bf5ae2d73285efe16b02
qvotaxon <2746261+qvotaxon@users.noreply.github.com>
2024-06-28 21:47:33 +0200

    refactor: update configuration wizard logic
    Refactor configuration wizard to use new i18nextScannerModuleConfiguration interface. Replace configuration steps with new async methods for file extensions, languages, namespaces, translation functions. Update 
    configuration storage to use ConfigurationStoreManager.

--- src/lib/services/configurationWizard/configurationWizardService.ts
+++ src/lib/services/configurationWizard/configurationWizardService.ts
@@ -1,6 +1,7 @@
 import vscode from 'vscode';
 
-import ExtensionConfiguration from '../../entities/configuration/extensionConfiguration';
+import I18nextScannerModuleConfiguration from '../../entities/configuration/modules/i18nextScanner/i18nextScannerModuleConfiguration';
+import ConfigurationStoreManager from '../../stores/configuration/configurationStoreManager';
 import {
   promptForFolder as promptForFolderAsync,
   promptForFolders as promptForFoldersAsync,
@@ -16,10 +17,10 @@ enum Framework {
 
 export default class ConfigurationWizardService {
   public async startConfigurationWizardAsync(): Promise<
-    Partial<ExtensionConfiguration> | undefined
+    Partial<I18nextScannerModuleConfiguration> | undefined
   > {
-    const config: Partial<ExtensionConfiguration> =
-      new ExtensionConfiguration();
+    const i18nextScannerModuleConfiguration: I18nextScannerModuleConfiguration =
+      new I18nextScannerModuleConfiguration();
 
     const framework = await this.selectFramework();
 
@@ -28,22 +29,67 @@ export default class ConfigurationWizardService {
     }
 
     if (framework === Framework.Custom) {
-      if (!(await this.configureTranslationFilesLocationAsync(config))) {
+      if (
+        !(await this.configureTranslationFilesLocationAsync(
+          i18nextScannerModuleConfiguration
+        ))
+      ) {
         return undefined;
       }
 
-      if (!(await this.configureCodeFileLocations(config))) {
+      if (
+        !(await this.configureCodeFileLocationsAsync(
+          i18nextScannerModuleConfiguration
+        ))
+      ) {
+        return undefined;
+      }
+
+      if (
+        !(await this.configureFileExtensionsAsync(
+          i18nextScannerModuleConfiguration
+        ))
+      ) {
         return undefined;
       }
     } else {
       //TODO: implement specific logic for each framework
     }
 
-    if (!(await this.configureDefaultLanguageAsync(config))) {
+    if (
+      !(await this.configureDefaultLanguageAsync(
+        i18nextScannerModuleConfiguration
+      ))
+    ) {
       return undefined;
     }
 
-    return config;
+    if (
+      !(await this.configureLanguagesAsync(i18nextScannerModuleConfiguration))
+    ) {
+      return undefined;
+    }
+
+    if (
+      !(await this.configureNamespacesAsync(i18nextScannerModuleConfiguration))
+    ) {
+      return undefined;
+    }
+
+    if (
+      !(await this.configureTranslationFunctionNamesAsync(
+        i18nextScannerModuleConfiguration
+      ))
+    ) {
+      return undefined;
+    }
+
+    await ConfigurationStoreManager.getInstance().setConfigAsync<I18nextScannerModuleConfiguration>(
+      'i18nextScannerModule',
+      i18nextScannerModuleConfiguration
+    );
+
+    return i18nextScannerModuleConfiguration;
   }
 
   private async selectFramework(): Promise<string | undefined> {
@@ -53,7 +99,7 @@ export default class ConfigurationWizardService {
   }
 
   private async configureTranslationFilesLocationAsync(
-    config: Partial<ExtensionConfiguration>
+    i18nextScannerModuleConfiguration: Partial<I18nextScannerModuleConfiguration>
   ): Promise<boolean> {
     const translationFilesLocation = await promptForFolderAsync(
       'Select the translation files location folder'
@@ -62,14 +108,14 @@ export default class ConfigurationWizardService {
       return false;
     }
 
-    config.i18nextScannerModule!.translationFilesLocation =
+    i18nextScannerModuleConfiguration!.translationFilesLocation =
       translationFilesLocation;
 
     return true;
   }
 
-  private async configureCodeFileLocations(
-    config: Partial<ExtensionConfiguration>
+  private async configureCodeFileLocationsAsync(
+    i18nextScannerModuleConfiguration: Partial<I18nextScannerModuleConfiguration>
   ): Promise<boolean> {
     const codeFileLocations = await promptForFoldersAsync(
       'Select the code file locations folders'
@@ -78,13 +124,13 @@ export default class ConfigurationWizardService {
       return false;
     }
 
-    config.i18nextScannerModule!.codeFileLocations = codeFileLocations;
+    i18nextScannerModuleConfiguration.codeFileLocations = codeFileLocations;
 
     return true;
   }
 
   private async configureDefaultLanguageAsync(
-    config: Partial<ExtensionConfiguration>
+    i18nextScannerModuleConfiguration: Partial<I18nextScannerModuleConfiguration>
   ): Promise<boolean> {
     const defaultLanguage = await vscode.window.showInputBox({
       placeHolder: 'For example: en or fr',
@@ -95,7 +141,77 @@ export default class ConfigurationWizardService {
       return false; // User cancelled
     }
 
-    config.i18nextScannerModule!.defaultLanguage = defaultLanguage;
+    i18nextScannerModuleConfiguration!.defaultLanguage = defaultLanguage;
+
+    return true;
+  }
+
+  private async configureFileExtensionsAsync(
+    i18nextScannerModuleConfiguration: Partial<I18nextScannerModuleConfiguration>
+  ): Promise<boolean> {
+    const fileExtensions = await vscode.window.showInputBox({
+      placeHolder: 'For example: ts, tsx, js, jsx',
+      prompt: 'Enter the file extensions to scan',
+    });
+
+    if (!fileExtensions) {
+      return false; // User cancelled
+    }
+
+    i18nextScannerModuleConfiguration!.fileExtensions =
+      fileExtensions.split(', ');
+
+    return true;
+  }
+
+  private async configureLanguagesAsync(
+    i18nextScannerModuleConfiguration: Partial<I18nextScannerModuleConfiguration>
+  ): Promise<boolean> {
+    const languages = await vscode.window.showInputBox({
+      placeHolder: 'For example: en, fr, de',
+      prompt: 'Enter the languages of your application',
+    });
+
+    if (!languages) {
+      return false; // User cancelled
+    }
+
+    i18nextScannerModuleConfiguration!.languages = languages.split(', ');
+
+    return true;
+  }
+
+  private async configureNamespacesAsync(
+    i18nextScannerModuleConfiguration: Partial<I18nextScannerModuleConfiguration>
+  ): Promise<boolean> {
+    const namespaces = await vscode.window.showInputBox({
+      placeHolder: 'For example: common, auth',
+      prompt: 'Enter the namespaces of your application',
+    });
+
+    if (!namespaces) {
+      return false; // User cancelled
+    }
+
+    i18nextScannerModuleConfiguration!.namespaces = namespaces.split(', ');
+
+    return true;
+  }
+
+  private async configureTranslationFunctionNamesAsync(
+    i18nextScannerModuleConfiguration: Partial<I18nextScannerModuleConfiguration>
+  ): Promise<boolean> {
+    const translationFunctionNames = await vscode.window.showInputBox({
+      placeHolder: 'For example: t, i18next.t',
+      prompt: 'Enter the translation function names',
+    });
+
+    if (!translationFunctionNames) {
+      return false; // User cancelled
+    }
+
+    i18nextScannerModuleConfiguration!.translationFunctionNames =
+      translationFunctionNames.split(', ');
 
     return true;
   }
