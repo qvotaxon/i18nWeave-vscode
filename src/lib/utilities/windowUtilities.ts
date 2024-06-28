671c72f46fe403d56bbac48710f5d06d563d830c
qvotaxon <2746261+qvotaxon@users.noreply.github.com>
2024-06-28 21:44:43 +0200

    fix(util): fix folder selection logic & add doc comments
    Update folderUri length check from >0 to >1 to correctly evaluate 
    multiple folder selections. Add JSDoc comments to 'promptForFolders' 
    and 'showOpenDialog' for better clarity and maintainability. 
    Refine showOpenDialog to include a user prompt before folder selection.

--- src/lib/utilities/windowUtilities.ts
+++ src/lib/utilities/windowUtilities.ts
@@ -11,16 +11,37 @@ export async function promptForFolder(
   return (await showOpenDialog(placeHolder)) as string;
 }
 
+/**
+ * Prompts the user to select one or more folders.
+ *
+ * @param placeHolder - The placeholder text to display in the folder selection dialog.
+ * @returns A promise that resolves to an array of selected folder paths, or undefined if the user cancels the selection.
+ */
 export async function promptForFolders(
   placeHolder: string
 ): Promise<string[] | undefined> {
   return (await showOpenDialog(placeHolder, true)) as string[];
 }
 
+/**
+ * Displays an open dialog to select a folder.
+ * @param placeHolder - The placeholder text to display in the dialog.
+ * @param canSelectMany - Optional. Specifies whether multiple folders can be selected. Default is false.
+ * @returns A promise that resolves to the selected folder path(s) or undefined if no folder is selected.
+ */
 async function showOpenDialog(
   placeHolder: string,
   canSelectMany = false
 ): Promise<string | string[] | undefined> {
+  const translationFilesFolderSelectionPrompt =
+    await vscode.window.showQuickPick([placeHolder], {
+      placeHolder: placeHolder,
+    });
+
+  if (!translationFilesFolderSelectionPrompt) {
+    return undefined;
+  }
+
   const folderUri = await vscode.window.showOpenDialog({
     canSelectFolders: true,
     canSelectMany: canSelectMany,
@@ -32,7 +53,7 @@ async function showOpenDialog(
     return undefined;
   }
 
-  if (folderUri.length > 0) {
+  if (folderUri.length > 1) {
     return folderUri.map(folderUri => folderUri.fsPath);
   }
 
