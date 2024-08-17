cde4d96815b8fbf88547f6d91142dadf5a018f7f
qvotaxon <2746261+qvotaxon@users.noreply.github.com>
2024-08-17 16:25:00 +0200

    refactor: remove read-po-file module and its references
    Remove `read-po-file` module and its test files to streamline
    the codebase. This module is no longer needed as it has been
    replaced by more efficient code. Updated tsconfig.json to
    reflect this change by removing related path mappings.

--- src/libs/module/module-read-po-file/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './lib/read-po-file-module';
-export * from './lib/read-po-file-module-context';
--- src/libs/module/module-read-po-file/src/lib/read-po-file-module-context.ts
+++
@@ -1,5 +0,0 @@
-import { BaseModuleContext } from '@i18n-weave/module/module-base-action';
-
-export abstract class ReadPoFileModuleContext extends BaseModuleContext {
-  poContent: any;
-}
