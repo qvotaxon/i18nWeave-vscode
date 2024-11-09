ea5f596bd96fea016927878d46862ea49e60420c
qvotaxon <2746261+qvotaxon@users.noreply.github.com>
2024-11-09 16:45:18 +0100

    refactor(cache-keys): remove unused project-specific keys
    Remove project-specific cache keys due to redundancy and lack of usage across the codebase. This simplification helps maintain the focus on relevant shared cache keys, enhancing maintainability.

--- src/libs/feature/feature-caching-service/src/lib/cache-keys.ts
+++ src/libs/feature/feature-caching-service/src/lib/cache-keys.ts
@@ -1,15 +1,3 @@
-import { getProjectRootFolder } from '@i18n-weave/util/util-file-path-utilities';
-
 export const sharedCacheKeys = {
   SUPPORTED_TARGET_LANGUAGES: 'i18nWeave.shared.supportedTargetLanguages',
-};
-
-export const projectSpecificCacheKeys = () => {
-  const projectRootFolder = getProjectRootFolder();
-
-  const projectNameHash = Buffer.from(projectRootFolder).toString('base64');
-
-  return {
-    TRANSLATION_FUNCTION: `i18nWeave.${projectNameHash}.translationFunctionCache`,
-  };
 };
