efaac6ec947a47a6e1840174d03d0a1cc80f5316
qvotaxon <2746261+qvotaxon@users.noreply.github.com>
2024-11-10 13:22:55 +0100

    refactor: simplify diffJsonObjects and add applyChange

--- src/libs/util/util-file-diff/src/lib/file-diff.ts
+++ src/libs/util/util-file-diff/src/lib/file-diff.ts
@@ -1,18 +1,18 @@
-import deepDiff from 'deep-diff';
+import deepDiff, { Diff } from 'deep-diff';
 
 export function diffJsonObjects(
   oldObj: object,
   newObj: object
-): { additions: any[]; deletions: any[]; updates: any[] } {
-  const differences = deepDiff.diff(oldObj, newObj);
-
-  const additions = differences?.filter(d => d.kind === 'N') || [];
-  const deletions = differences?.filter(d => d.kind === 'D') || [];
-  const updates = differences?.filter(d => d.kind === 'E') || [];
-
-  return {
-    additions,
-    deletions,
-    updates,
-  };
+): deepDiff.Diff<object, object>[] | undefined {
+  return deepDiff.diff(oldObj, newObj);
 }
+
+export function applyChange(jsonObj: JSON, diffs: Diff<any, any>[]): JSON {
+  diffs.forEach(diff => {
+    deepDiff.applyChange(jsonObj, {}, diff);
+    var test = jsonObj;
+  });
+
+  return jsonObj;
+}
+
