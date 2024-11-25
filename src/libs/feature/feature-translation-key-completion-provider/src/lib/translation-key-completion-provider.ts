7cf18b4669b6d7e7b0d6a74b8480f4ad0d151b8c
qvotaxon <2746261+qvotaxon@users.noreply.github.com>
2024-11-25 17:23:33 +0100

    fix(completion-provider): enhance completion logic
    Refactor code to separate key retrieval logic and streamline the 
    transformation of translation files for better clarity. Introduce 
    word range detection for precise replacement of translation keys. 
    
    Add `resolveCompletionItem` for dynamic detail and documentation 
    updates. Integrate configuration for custom separators to trigger 
    provider in extension activation routine.

--- src/libs/feature/feature-translation-key-completion-provider/src/lib/translation-key-completion-provider.ts
+++ src/libs/feature/feature-translation-key-completion-provider/src/lib/translation-key-completion-provider.ts
@@ -25,6 +25,42 @@ export class TranslationKeyCompletionProvider {
     return TranslationKeyCompletionProvider._instance;
   }
 
+  public resolveCompletionItem?(
+    item: vscode.CompletionItem,
+    _: vscode.CancellationToken
+  ): vscode.ProviderResult<vscode.CompletionItem> {
+    const fileLocationStore = FileLocationStore.getInstance();
+    const configuration =
+      ConfigurationStoreManager.getInstance().getConfig<I18nextScannerModuleConfiguration>(
+        'i18nextScannerModule'
+      );
+
+    if (item.detail) {
+      const itemData = JSON.parse(item.detail.toString()) as {
+        namespace: string;
+        translationKey: string;
+      };
+      const namespace = itemData.namespace;
+      const translationKey = itemData.translationKey;
+
+      const translationValue = fileLocationStore
+        .getTranslationFiles()
+        .filter(
+          x =>
+            x.language === configuration.defaultLanguage &&
+            x.namespace === namespace
+        )
+        .filter(x => x.keys[translationKey.toString()].value)
+        .map(x => x.keys[translationKey].value)[0];
+
+      item.documentation = `${translationValue}`;
+      item.detail = `Key: ${translationKey} (Namespace: ${namespace})}`;
+      // item.label = `${translationKey} - ${translationValue}`;
+    }
+
+    return item;
+  }
+
   public provideCompletionItems(
     document: vscode.TextDocument,
     position: vscode.Position
@@ -52,31 +88,48 @@ export class TranslationKeyCompletionProvider {
 
     const keyPrefix = match[2];
 
-    const [namespace, key] = keyPrefix.includes(configuration.nsSeparator)
+    const [namespace, _] = keyPrefix.includes(configuration.nsSeparator)
       ? keyPrefix.split(configuration.nsSeparator)
       : [configuration.defaultNamespace, keyPrefix];
 
-    return fileLocationStore
+    const translationFiles = fileLocationStore
       .getTranslationFiles()
-      .filter(x => x.language === configuration.defaultLanguage)
-      .filter(x => x.namespace === namespace)
-      .reduce((acc, file) => {
-        return acc.concat(Object.keys(file.keys));
-      }, [] as string[])
       .filter(
-        k =>
-          k.startsWith(key) &&
-          !k.includes(configuration.contextSeparator) &&
-          !k.includes(configuration.pluralSeparator)
+        x =>
+          x.language === configuration.defaultLanguage &&
+          x.namespace === namespace
+      );
+
+    const keys = translationFiles.flatMap(file => Object.keys(file.keys));
+
+    return keys
+      .filter(
+        translationKey =>
+          translationKey.startsWith(translationKey) &&
+          !translationKey.includes(configuration.contextSeparator) &&
+          !translationKey.includes(configuration.pluralSeparator)
       )
-      .map(k => {
+      .map(translationKey => {
         const item = new vscode.CompletionItem(
-          k,
+          translationKey,
           vscode.CompletionItemKind.Text
         );
-        item.insertText = k.substring(key.length);
-        item.detail = `Translation key ${k} - i18nWeave`;
-        item.documentation = `This is a translation key ${k}`;
+
+        item.detail = `{"namespace":"${namespace}", "translationKey":"${translationKey}"}`;
+
+        const wordRange = document.getWordRangeAtPosition(position, /[\w\.]+/); // Match alphanumeric or dots
+        if (wordRange) {
+          // Step 2: Set the range for replacement to the detected word range
+          item.range = wordRange;
+
+          // Step 3: Specify the text to insert
+          item.insertText = translationKey;
+        } else {
+          // Step 4: If no word range is detected, handle insertion at the cursor position
+          item.range = new vscode.Range(position, position); // Insert at cursor only
+          item.insertText = translationKey;
+        }
+
         return item;
       });
   }
