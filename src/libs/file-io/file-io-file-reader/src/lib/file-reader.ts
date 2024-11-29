91150d2e6f05c2bbee7491721a27019f012609da
qvotaxon <2746261+qvotaxon@users.noreply.github.com>
2024-11-29 21:35:13 +0100

    refactor: convert method to non-static and inject fs
    Refactor FileReader to use dependency injection for the fs
    module, allowing easier testing and customization. Update
    readWorkspaceFileAsync method to be non-static, improving
    flexibility. Make Sentry usage optional by using safe chaining.

--- src/libs/file-io/file-io-file-reader/src/lib/file-reader.ts
+++ src/libs/file-io/file-io-file-reader/src/lib/file-reader.ts
@@ -5,17 +5,19 @@ import { Uri, workspace } from 'vscode';
  * Utility class for reading files asynchronously.
  */
 export class FileReader {
+  constructor(private fs = workspace.fs) {}
+
   /**
    * Reads the contents of a file asynchronously.
    * @param filePath - The path to the file to be read.
    * @returns A promise that resolves with the contents of the file as a string.
    */
-  public static async readWorkspaceFileAsync(filePath: Uri): Promise<string> {
+  public async readWorkspaceFileAsync(filePath: Uri): Promise<string> {
     try {
-      const fileData = await workspace.fs.readFile(filePath);
+      const fileData = await this.fs.readFile(filePath);
       return new TextDecoder().decode(fileData);
     } catch (error) {
-      Sentry.captureException(error);
+      Sentry?.captureException(error);
       return Promise.reject(error as Error);
     }
   }
