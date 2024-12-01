0593125671ababff5dfd1534e26dfc6370bf8189
qvotaxon <2746261+qvotaxon@users.noreply.github.com>
2024-12-01 11:42:26 +0100

    fix: remove dummy fix
    Remove dummy fix

--- src/core/extension.ts
+++ src/core/extension.ts
@@ -42,8 +42,6 @@ function initializeSentry() {
   )!;
   const installationId = vscode.env.machineId;
 
-  console.log(`Installation id: ${installationId}`);
-
   Sentry.init({
     enabled:
       isProduction() &&
